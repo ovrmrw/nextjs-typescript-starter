603f153e1468f68cd7d5548b78b5fb2a14e4ab91
Tomohiro Noguchi <t.ngc.0403@gmail.com>
2021-05-13 13:15:02 +0900

    Initial commit

---
+++ prettier.config.js
@@ -0,0 +1,10 @@
+module.exports = {
+  printWidth: 120,
+  trailingComma: 'all',
+  useTabs: false,
+  tabWidth: 2,
+  semi: true,
+  singleQuote: true,
+  jsxSingleQuote: false,
+  jsxBracketSameLine: true,
+};
